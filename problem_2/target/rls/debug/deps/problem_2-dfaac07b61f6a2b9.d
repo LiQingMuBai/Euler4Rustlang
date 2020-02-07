a93df17503872ab5a014ff517c5001064c8d39b4
SherlockHolmes2020 <60126110+SherlockHolmes2020@users.noreply.github.com>
2020-02-07 09:17:21 -0500

    fix problem_002

--- problem_2/target/rls/debug/deps/problem_2-dfaac07b61f6a2b9.d
+++
@@ -1,5 +0,0 @@
-/Users/sherlock/source/rust_projects/Euler4Rustlang/problem_2/target/rls/debug/deps/problem_2-dfaac07b61f6a2b9.rmeta: src/main.rs
-
-/Users/sherlock/source/rust_projects/Euler4Rustlang/problem_2/target/rls/debug/deps/problem_2-dfaac07b61f6a2b9.d: src/main.rs
-
-src/main.rs:
